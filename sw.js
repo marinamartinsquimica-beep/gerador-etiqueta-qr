0e7be81ddfc32c971488e1e03927a82ca4a5cc5f
marinamartinsquimica-beep <marinamartinsquimica@gmail.com>
2026-08-17 09:01:50 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'gerador-qr-paletes-v1.0.6-lote-curto';
+const CACHE_NAME = 'gerador-qr-paletes-v1.0.7-tres-etiquetas';
 const ASSETS = ['./', './index.html', './styles.css', './app.js', './manifest.webmanifest', './version.json', './vendor/qrcode.min.js', './icons/icon-192.png', './icons/icon-512.png'];
 
 self.addEventListener('install', (event) => event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))));
